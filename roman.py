210ea3502ae4ee95dd62c7220f1f2b336ac0a598
unknown <celis.hernandez.cesar@gmail.com>
2019-12-31 20:03:45 -0500

    changing dictionary to tuple as per review

--- roman.py
+++ roman.py
@@ -1,10 +1,9 @@
 import re
 
+ROMAN_NUMBERS = (('I',1),('V',5),('X',10),('L',50))
+
 def roman_to_integer(value):
-    number_dict = {'I':1, "V":5, "X":10, "L":50}
     total = 0
-    i_sum = value.count("I")
-    v_sum = value.count("V")
 
     if 'I' in value:
         pattern = '.*I[V,X,L]+'
@@ -13,5 +12,7 @@ def roman_to_integer(value):
             #remove I from string
             value = value.replace("I", "")
     for c in value:
-         total += number_dict[c]
+         for item in ROMAN_NUMBERS:
+             if item[0] == c:
+                 total += item[1]
     return total
